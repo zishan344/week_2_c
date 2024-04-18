834040a719fdbff5aff984b5cfb5a7f78101fe4b
zishan344 <zishanahmed344@gmail.com>
2024-04-18 23:24:38 +0600

    coplete assignemnt 2 pb-3

---
+++ assignment2/Reverse_and_Odd.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+   int arrSize;
+   scanf("%d", &arrSize);
+   int arr[arrSize];
+   for (int i = 0; i <arrSize; i++)
+   {
+    int arrValue;
+    scanf("%d", &arrValue);
+    arr[i]=arrValue;
+   };
+   
+   for (int i = arrSize-1; i >= 0; i--)
+   {
+  
+    if(i%2!=0) printf("%d ", arr[i]);
+   };
+   
+   
+  return 0;
+}
